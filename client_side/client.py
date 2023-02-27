b12019576d361d6a4e5d66e01e1d1ba132f29e75
Datremar <mr.protoss@bk.ru>
2023-02-27 18:40:19 +0700

    Made client launch from the console.

--- client_side/client.py
+++ client_side/client.py
@@ -1,12 +1,15 @@
 import socket
+from sys import argv
+
 
 HOST = "127.0.0.1"
 PORT = 65432
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        expression = argv[1]
         s.connect((HOST, PORT))
-        s.sendall(b"Hello, world")
+        s.sendall(expression.encode(encoding="utf-8"))
         data = s.recv(1024)
 
-    print(f"Received {data!r}")
+    print("{}".format(data.decode(encoding="utf-8")))
