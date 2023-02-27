a5abb33af282a9ef408596dcd97284e5a6ddb747
Datremar <mr.protoss@bk.ru>
2023-02-27 16:59:31 +0700

    Initial commit. Created prototypes for client and server socket interactions.

---
+++ server_side/server.py
@@ -0,0 +1,41 @@
+import socket
+
+
+class ServerHandler:
+    _HOST = "127.0.0.1"
+    _PORT = 65432
+
+    @staticmethod
+    def solve(expression):
+        return str(eval(expression))
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self._HOST, self._PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(data)
+                    conn.sendall(data)
+
+
+class Server:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not Server._instance:
+            Server._instance = ServerHandler()
+
+        return Server._instance
+
+
+if __name__ == "__main__":
+    print(type(eval("2 - 1.3")))
