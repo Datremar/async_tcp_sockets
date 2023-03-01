df410d68ee2deeb89e0979960ed84565130fa48b
Datremar <mr.protoss@bk.ru>
2023-03-01 14:15:58 +0700

    Implemented request/response mechanism for easier payload packaging

---
+++ client_side/utils.py
@@ -0,0 +1,11 @@
+from json import dumps, loads
+
+
+class Request:
+    def __new__(cls, payload) -> bytes:
+        return dumps(payload).encode(encoding="utf-8")
+
+
+class Response:
+    def __new__(cls, payload: bytes) -> dict:
+        return loads(payload.decode(encoding="utf-8"))
