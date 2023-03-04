b777a2d088ba47d648ffddfbddb9883f1e99eaf2
Datremar <mr.protoss@bk.ru>
2023-03-04 14:22:59 +0700

    The client now has logging and can run indefinitely until you enter "exit" command as an input.

--- client_side/client.py
+++ client_side/client.py
@@ -1,7 +1,8 @@
 import asyncio
+import logging
 
+from json import dumps
 from socket import socket, AF_INET, SOCK_STREAM
-from sys import argv
 
 from utils import Request, Response
 
@@ -9,10 +10,26 @@ from utils import Request, Response
 class Client:
     _HOST = "127.0.0.1"
     _PORT = 65432
-    semaphore = asyncio.Semaphore(value=500)
+    _semaphore_value = 500
+
+    def __init__(self):
+        self._semaphore = asyncio.Semaphore(value=Client._semaphore_value)
+
+        logging.basicConfig(
+            filename='./logs.log',
+            format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+            level=logging.DEBUG
+        )
+        logging.info("Initializing client")
+        logging.debug("Client params:\n- host {}\n- port {}\n- semaphore value {}\n".format(
+            Client._HOST,
+            Client._PORT,
+            Client._semaphore_value
+        ))
 
     async def request(self, expression: str):
-        async with Client.semaphore:
+        logging.info("Making request {} to: {}:{}".format(expression, Client._HOST, Client._PORT))
+        async with self._semaphore:
             loop = asyncio.get_event_loop()
 
             s = socket(AF_INET, SOCK_STREAM)
@@ -25,8 +42,32 @@ class Client:
             await loop.sock_connect(s, (self._HOST, self._PORT))
             await loop.sock_sendall(s, request)
 
-            return Response(await loop.sock_recv(s, 100000))
+            response = Response(await loop.sock_recv(s, 100000))
+
+            logging.info("Getting response: {} from {}:{}".format(dumps(response), Client._HOST, Client._PORT))
+            s.close()
+
+            return response
+
+    def run(self):
+        logging.info("Starting ")
+        print("Enter the expression to calculate on the server.\nIf you wish to exit the client, enter 'exit' as an "
+              "input.")
+        while True:
+            expression = input()
+            if expression == "exit":
+                break
+
+            try:
+                response = asyncio.run(self.request(expression))
+            except Exception as e:
+                print("EXCEPTION OCCURED:", str(e))
+                logging.error("EXCEPTION OCCURED:\n{}".format(str(e)))
+
+                continue
+
+            print("Response:", response)
 
 
 if __name__ == "__main__":
-    print(asyncio.run(Client().request(argv[1])))
+    Client().run()
